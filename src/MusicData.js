32e74708941effdd18a045c24db4d25ffef3a929
Kyle Floros <kylefloros@gmail.com>
2019-11-17 20:19:06 -0600

    replaced flat/sharp symbols with b/#

--- src/MusicData.js
+++ src/MusicData.js
@@ -1,93 +1,93 @@
 export const notesLookup = {
   c: {
     major: ["C", "D", "E", "F", "G", "A", "B"],
-    natural_minor: ["C", "D", "E♭", "F", "G", "A♭", "B♭"],
-    harmonic_minor: ["C", "D", "E♭", "F", "G", "A♭", "B"]
+    natural_minor: ["C", "D", "Eb", "F", "G", "Ab", "Bb"],
+    harmonic_minor: ["C", "D", "Eb", "F", "G", "Ab", "B"]
   },
   g: {
-    major: ["G", "A", "B", "C", "D", "E", "F♯"],
-    natural_minor: ["G", "A", "B♭", "C", "D", "E♭", "F", "G"],
-    harmonic_minor: ["G", "A", "B♭", "C", "D", "E♭", "F♯"]
+    major: ["G", "A", "B", "C", "D", "E", "F#"],
+    natural_minor: ["G", "A", "Bb", "C", "D", "Eb", "F", "G"],
+    harmonic_minor: ["G", "A", "Bb", "C", "D", "Eb", "F#"]
   },
   d: {
-    major: ["D", "E", "F♯", "G", "A", "B", "C♯"],
-    natural_minor: ["D", "E", "F", "G", "A", "B♭", "C", "D"],
-    harmonic_minor: ["D", "E", "F", "G", "A", "B♭", "C♯"]
+    major: ["D", "E", "F#", "G", "A", "B", "C#"],
+    natural_minor: ["D", "E", "F", "G", "A", "Bb", "C", "D"],
+    harmonic_minor: ["D", "E", "F", "G", "A", "Bb", "C#"]
   },
   a: {
-    major: ["A", "B", "C♯", "D", "E", "F♯", "G♯"],
+    major: ["A", "B", "C#", "D", "E", "F#", "G#"],
     natural_minor: ["A", "B", "C", "D", "E", "F", "G", "A"],
-    harmonic_minor: ["A", "B", "C", "D", "E", "F", "G♯"]
+    harmonic_minor: ["A", "B", "C", "D", "E", "F", "G#"]
   },
   e: {
-    major: ["E", "F♯", "G♯", "A", "B", "C♯", "D♯"],
-    natural_minor: ["E", "F♯", "G", "A", "B", "C", "D", "E"],
-    harmonic_minor: ["E", "F♯", "G", "A", "B", "C", "D♯"]
+    major: ["E", "F#", "G#", "A", "B", "C#", "D#"],
+    natural_minor: ["E", "F#", "G", "A", "B", "C", "D", "E"],
+    harmonic_minor: ["E", "F#", "G", "A", "B", "C", "D#"]
   },
   b: {
-    major: ["B", "C♯", "D♯", "E", "F♯", "G♯", "A♯"],
-    natural_minor: ["B", "C♯", "D", "E", "F♯", "G", "A", "B"],
-    harmonic_minor: ["B", "C♯", "D", "E", "F♯", "G", "A♯"]
+    major: ["B", "C#", "D#", "E", "F#", "G#", "A#"],
+    natural_minor: ["B", "C#", "D", "E", "F#", "G", "A", "B"],
+    harmonic_minor: ["B", "C#", "D", "E", "F#", "G", "A#"]
   },
   f_sharp: {
-    major: ["F♯", "G♯", "A♯", "B", "C♯", "D♯", "E♯"],
-    natural_minor: ["F♯", "G♯", "A", "B", "C♯", "D", "E", "F♯"],
-    harmonic_minor: ["F♯", "G♯", "A", "B", "C♯", "D", "E♯"]
+    major: ["F#", "G#", "A#", "B", "C#", "D#", "E#"],
+    natural_minor: ["F#", "G#", "A", "B", "C#", "D", "E", "F#"],
+    harmonic_minor: ["F#", "G#", "A", "B", "C#", "D", "E#"]
   },
   d_flat: {
-    major: ["D♭", "E♭", "F", "G♭", "A♭", "B♭", "C"],
-    natural_minor: ["C♯", "D♯", "E", "F♯", "G♯", "A", "B", "C♯"],
-    harmonic_minor: ["D♭", "E♭", "F♭", "G♭", "A♭", "B♭♭", "C"]
+    major: ["Db", "Eb", "F", "Gb", "Ab", "Bb", "C"],
+    natural_minor: ["C#", "D#", "E", "F#", "G#", "A", "B", "C#"],
+    harmonic_minor: ["Db", "Eb", "Fb", "Gb", "Ab", "Bbb", "C"]
   },
   a_flat: {
-    major: ["A♭", "B♭", "C", "D♭", "E♭", "F", "G"],
-    natural_minor: ["G♯", "A♯", "B", "C♯", "D♯", "E", "F♯", "G♯"],
-    harmonic_minor: ["A♭", "B♭", "C♭", "D♭", "E♭", "F♭", "G"]
+    major: ["Ab", "Bb", "C", "Db", "Eb", "F", "G"],
+    natural_minor: ["G#", "A#", "B", "C#", "D#", "E", "F#", "G#"],
+    harmonic_minor: ["Ab", "Bb", "Cb", "Db", "Eb", "Fb", "G"]
   },
   e_flat: {
-    major: ["E♭", "F", "G", "A♭", "B♭", "C", "D"],
-    natural_minor: ["E♭", "F", "G♭", "A♭", "B♭", "C♭", "D♭", "E♭"],
-    harmonic_minor: ["E♭", "F", "G♭", "A♭", "B♭", "C♭", "D"]
+    major: ["Eb", "F", "G", "Ab", "Bb", "C", "D"],
+    natural_minor: ["Eb", "F", "Gb", "Ab", "Bb", "Cb", "Db", "Eb"],
+    harmonic_minor: ["Eb", "F", "Gb", "Ab", "Bb", "Cb", "D"]
   },
   b_flat: {
-    major: ["B♭", "C", "D", "E♭", "F", "G", "A"],
-    natural_minor: ["B♭", "C", "D♭", "E♭", "F", "G♭", "A♭", "B♭"],
-    harmonic_minor: ["B♭", "C", "D♭", "E♭", "F", "G♭", "A"]
+    major: ["Bb", "C", "D", "Eb", "F", "G", "A"],
+    natural_minor: ["Bb", "C", "Db", "Eb", "F", "Gb", "Ab", "Bb"],
+    harmonic_minor: ["Bb", "C", "Db", "Eb", "F", "Gb", "A"]
   },
   f: {
-    major: ["F", "G", "A", "B♭", "C", "D", "E"],
-    natural_minor: ["F", "G", "A♭", "B♭", "C", "D♭", "E♭", "F"],
-    harmonic_minor: ["F", "G", "A♭", "B♭", "C", "D♭", "E"]
+    major: ["F", "G", "A", "Bb", "C", "D", "E"],
+    natural_minor: ["F", "G", "Ab", "Bb", "C", "Db", "Eb", "F"],
+    harmonic_minor: ["F", "G", "Ab", "Bb", "C", "Db", "E"]
   }
 };
 export const chordsLookup = {
   major: {
     triad: ["", "m", "m", "", "", "m", "dim"],
-    tetrad: ["maj7", "m7", "m7", "maj7", "7", "m7", "m7(♭5)"]
+    tetrad: ["maj7", "m7", "m7", "maj7", "7", "m7", "m7(b5)"]
   },
   natural_minor: {
     triad: ["m", "dim", "", "m", "m", "", ""],
-    tetrad: ["min7", "m7(♭5)", "maj7", "min7", "min7", "maj7", "7"]
+    tetrad: ["min7", "m7(b5)", "maj7", "min7", "min7", "maj7", "7"]
   },
   harmonic_minor: {
     triad: ["m", "dim", "aug", "m", "", "", "dim"],
-    tetrad: ["m/M7", "m7(♭5)", "augM7", "min7", "7", "maj7", "dim7"]
+    tetrad: ["m/M7", "m7(b5)", "augM7", "min7", "7", "maj7", "dim7"]
   }
 };
 
 export const keyLookup = {
   a: "A",
-  b_flat: "B♭",
+  b_flat: "Bb",
   b: "B",
   c: "C",
-  d_flat: "D♭",
+  d_flat: "Db",
   d: "D",
-  e_flat: "E♭",
+  e_flat: "Eb",
   e: "E",
   f: "F",
-  f_sharp: "F♯",
+  f_sharp: "F#",
   g: "G",
-  a_flat: "A♭"
+  a_flat: "Ab"
 };
 
 export const keyTypeLookup = {
--- src/components/KeyOptions/KeySelector.js
+++ src/components/KeyOptions/KeySelector.js
@@ -5,17 +5,17 @@ export default function Key() {
   return (
     <ul className="flex w-full flex-wrap justify-center mb-6 mt-4 md:mt-8">
       <KeyButton value="a">A</KeyButton>
-      <KeyButton value="b_flat">B♭</KeyButton>
+      <KeyButton value="b_flat">Bb</KeyButton>
       <KeyButton value="b">B</KeyButton>
       <KeyButton value="c">C</KeyButton>
-      <KeyButton value="d_flat">D♭</KeyButton>
+      <KeyButton value="d_flat">Db</KeyButton>
       <KeyButton value="d">D</KeyButton>
-      <KeyButton value="e_flat">E♭</KeyButton>
+      <KeyButton value="e_flat">Eb</KeyButton>
       <KeyButton value="e">E</KeyButton>
       <KeyButton value="f">F</KeyButton>
-      <KeyButton value="f_sharp">F♯</KeyButton>
+      <KeyButton value="f_sharp">F#</KeyButton>
       <KeyButton value="g">G</KeyButton>
-      <KeyButton value="a_flat">A♭</KeyButton>
+      <KeyButton value="a_flat">Ab</KeyButton>
     </ul>
   );
 }
